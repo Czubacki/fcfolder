24297975ecd573ac3d77a6bff86e55c8a63ac81a
Czubacki <filipczubacki@wp.pl>
2022-11-16 14:11:25 +0100

    wew1

--- class/User.class.php
+++ class/User.class.php
@@ -10,8 +10,20 @@ class User {
     {
         $this->login = $login;
         $this->password = $password;
-
+        $this->firstname = $firstname = "";
+        $this->lastname = $lastname = "";
+    
     }
-}
 
+    public function register() {
+        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
+        $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $db = new mysqli('localhost', 'root', '', 'loginForm');
+        $preparedQuery = $db->prepare($query);
+        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, $this->firstName, $this->lastName);
+        $preparedQuery->execute();
+    }
+
+
+}
 ?>
