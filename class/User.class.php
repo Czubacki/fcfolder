06d794b915ba1eb13d8f5e5cd22d198520eb80a1
Czubacki <filipczubacki@wp.pl>
2022-11-30 13:55:26 +0100

    5656

--- class/User.class.php
+++ class/User.class.php
@@ -1,18 +1,17 @@
 <?php
 class User {
     private int $id;
-    public mysqli $db;
+    private mysqli $db;
     private string $login;
     private string $password;
-    private string $firstname;
-    private string $lastname;
+    private string $firstName;
+    private string $lastName;
 
-    public function __construct(string $login, string $password)
-    {
+    public function __construct(string $login, string $password) {
         $this->login = $login;
         $this->password = $password;
-        $this->firstname = $firstname = "";
-        $this->lastname = $lastname = "";
+        $this->firstName = "";
+        $this->lastName = "";
         global $db;
         $this->db = &$db;
     }
@@ -20,43 +19,33 @@ class User {
     public function register() : bool {
         $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
         $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
-        $db = new mysqli('localhost', 'root', '', 'loginForm');
-        $preparedQuery = $db->prepare($query);
-        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, $this->firstName, $this->lastName);
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, 
+                                            $this->firstName, $this->lastName);
         $result = $preparedQuery->execute();
         return $result;
     }
 
     public function login() : bool {
-
-       $query = "SELECT * FROM user WHERE login = ?";
-       $db = new mysqli('localhost', 'root', '', 'loginForm');
-       $preparedQuery = $db->prepare($query);
-       $preparedQuery->bind_param('s', $this->login);
-       $preparedQuery->execute();
-       $result = $preparedQuery->get_result();
-       if($result->num_rows == 1) {
-        $row = $result->fetch_assoc();
-        $passwordHash = $row['password'];
-        if(password_verify($this->password, $passwordHash)) {
-            $this->id = $row['id'];
-            $this->firstname = $row['firstname'];
-            $this->lastname = $row['lastname'];
-            return true;
-        }
-        else {
+        $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('s', $this->login);
+        $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $passwordHash = $row['password'];
+            if(password_verify($this->password, $passwordHash)) {
+                $this->id = $row['id'];
+                $this->firstName = $row['firstName'];
+                $this->lastName = $row['lastName'];
+                return true;
+            } else {
+                return false;
+            }
+        } else {
             return false;
         }
-    } else {
-        return false;
-    }
-
-       
-
-
-
-
-
     }
     public function setFirstName(string $firstName) {
         $this->firstName = $firstName;
